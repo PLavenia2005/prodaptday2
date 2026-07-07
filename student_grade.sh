157846a0ee0e4d06f4ea2d97457b179fa0a93637
PLavenia2005 <lavenia.penumala@prodapt.com>
2026-07-07 17:15:48 +0530

    added student_grade.sh

---
+++ student_grade.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Enter marks for 5 subjects: "
+
+read -p "Subject 1: " s1
+read -p "Subject 2: " s2
+read -p "Subject 3: " s3
+read -p "Subject 4: " s4
+read -p "Subject 5: " s5
+
+total=$((s1+s2+s3+s4+s5))
+average=$((total/5))
+
+echo "---------------"
+echo "Total Marks : $total"
+echo "Average marks : $average"
+
+if [ $average -ge 90 ] && [ $average -le 100 ]
+then
+	echo "Grade : A"
+elif [ $average -ge 75 ]
+then
+	echo "Grade : B"
+elif [  $average -ge 60 ]
+then 
+	echo "Grade : C"
+elif [ $average -ge 50 ]
+then 
+	echo "Grade : D"
+else 
+	echo "Grade : Fail"
+fi
+
